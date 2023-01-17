2e31ebaddc2297f553b37cb0d04608203d32d1bf
Änjana Pathirana <anjanamadhushan44@gmail.com>
2023-01-17 17:05:41 +0530

    Get api for processes exceeding limit, init.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const wmi = require("node-wmi");
-
 const app = express();
+const fiftyPercent = 0.5;
 
 app.get("/processes", (req, res) => {
   wmi.Query(
@@ -9,16 +9,24 @@ app.get("/processes", (req, res) => {
       class: "Win32_Process",
       properties: ["Name", "WorkingSetSize"],
     },
-    function (error, result) {
-      if (error) {
-        res.status(500).send(error);
-      } else {
-        res.status(200).send(result);
-      }
+    function (error, processResults) {
+      wmi.Query(
+        {
+          class: "Win32_ComputerSystem",
+          properties: ["TotalPhysicalMemory"],
+        },
+        function (error, osResults) {
+          var totalMemory = osResults[0].TotalPhysicalMemory;
+          var memoryThreshold = totalMemory * fiftyPercent;
+          var highMemoryProcesses = processResults.filter(function (p) {
+            return p.WorkingSetSize > memoryThreshold;
+          });
+          res.json(highMemoryProcesses);
+        }
+      );
     }
   );
 });
-
 app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+  console.log("Server started on port 3000");
 });
