fe243119197c3090363efa236f25d48da6092761
Änjana Pathirana <anjanamadhushan44@gmail.com>
2023-01-19 13:42:37 +0530

    cors package init

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const wmi = require("node-wmi");
 const app = express();
 const fiftyPercent = 0.5;
+
+app.use(cors());
 
 app.get("/api/processes", (req, res) => {
   wmi.Query(
@@ -26,16 +29,6 @@ app.get("/api/processes", (req, res) => {
       );
     }
   );
-});
-
-//Access control allow origin
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3001");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
 });
 
 app.listen(3000, () => {
