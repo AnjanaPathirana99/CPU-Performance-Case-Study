d2cb4dec3a4a374bad5be87832f45dcd8fb3b9ec
Änjana Pathirana <anjanamadhushan44@gmail.com>
2023-01-20 09:25:37 +0530

    Set mapping key to ProcessId

--- frontend/src/components/cpuUsageView.js
+++ frontend/src/components/cpuUsageView.js
@@ -1,54 +1,3 @@
-// import React, { Component } from "react";
-// import axios from "axios";
-
-// class CpuUsageView extends Component {
-//   state = {
-//     cpuProcesses: [],
-//   };
-
-//   async componentDidMount() {
-//     const { data: cpuProcesses } = await axios.get(
-//       "http://localhost:3000/api/processes"
-//     );
-//     this.setState(cpuProcesses);
-//     console.log(cpuProcesses); //log
-//   }
-
-//   render() {
-//     const { length: count } = this.state.cpuProcesses;
-//     if (count === 0)
-//       return <p>There are no processes exceeding 50% of CPU memory</p>;
-//     return (
-//       <React.Fragment>
-//         <p>
-//           There are {this.state.cpuProcesses.length} processes exceeding 50% of
-//           CPU memory
-//         </p>
-//         <table className="cpuUsageViewTable">
-//           <thead>
-//             <tr>
-//               <th>Process</th>
-//               <th>CPU usage</th>
-//               <th>CPU Usage (%)</th>
-//             </tr>
-//           </thead>
-//           <tbody>
-//             {this.state.cpuProcesses.map((cpuProcess) => (
-//               <tr>
-//                 <td>{cpuProcess.Name}</td>
-//                 <td>{cpuProcess.WorkingSetSize}</td>
-//                 <td>{cpuProcess.WorkingSetSize}</td>
-//               </tr>
-//             ))}
-//           </tbody>
-//         </table>
-//       </React.Fragment>
-//     );
-//   }
-// }
-
-// export default CpuUsageView;
-
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
@@ -74,7 +23,7 @@ function ProcessTable() {
       </thead>
       <tbody>
         {processes.map((process) => (
-          <tr key={process.Name}>
+          <tr key={process.ProcessId}>
             <td>{process.Name}</td>
             <td>{process.WorkingSetSize}</td>
           </tr>
