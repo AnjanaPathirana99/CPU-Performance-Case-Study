83ac825670bf5bdc3e62a2d1d609b1001ed65b7f
Änjana Pathirana <anjanamadhushan44@gmail.com>
2023-01-26 15:29:37 +0530

    setInterval function init

--- Backend/server.js
+++ Backend/server.js
@@ -10,28 +10,32 @@ const socket = io.connect("http://localhost:3001"); //POrt number of the admin s
 
 app.use(cors());
 
-wmi.Query(
-  {
-    class: "Win32_Process",
-    properties: ["ProcessId", "Name", "WorkingSetSize"],
-  },
-  function (error, processResults) {
-    wmi.Query(
-      {
-        class: "Win32_ComputerSystem",
-        properties: ["TotalPhysicalMemory"],
-      },
-      function (error, osResults) {
-        var totalMemory = osResults[0].TotalPhysicalMemory;
-        var memoryThreshold = totalMemory * fiftyPercent * 0.02; //development purpose made this 0.01
-        var highMemoryProcesses = processResults.filter(function (p) {
-          return p.WorkingSetSize > memoryThreshold;
-        });
-        socket.emit("çpu_usage", highMemoryProcesses);
-      }
-    );
-  }
-);
+function checkCpuUsage() {
+  wmi.Query(
+    {
+      class: "Win32_Process",
+      properties: ["ProcessId", "Name", "WorkingSetSize"],
+    },
+    function (error, processResults) {
+      wmi.Query(
+        {
+          class: "Win32_ComputerSystem",
+          properties: ["TotalPhysicalMemory"],
+        },
+        function (error, osResults) {
+          var totalMemory = osResults[0].TotalPhysicalMemory;
+          var memoryThreshold = totalMemory * fiftyPercent * 0.02; //development purpose made this 0.01
+          var highMemoryProcesses = processResults.filter(function (p) {
+            return p.WorkingSetSize > memoryThreshold;
+          });
+          socket.emit("çpu_usage", highMemoryProcesses);
+        }
+      );
+    }
+  );
+}
+
+setInterval(checkCpuUsage, 1000);
 
 app.listen(3000, () => {
   console.log("Server started on port 3000");
